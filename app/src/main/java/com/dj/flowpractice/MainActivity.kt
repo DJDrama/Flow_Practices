433464b29d6d72bbe50b7e2d36b1434fb090fc10
DJDrama <putzzang@naver.com>
2021-08-11 23:15:33 +0900

    take for receving counted emissions

--- app/src/main/java/com/dj/flowpractice/MainActivity.kt
+++ app/src/main/java/com/dj/flowpractice/MainActivity.kt
@@ -6,14 +6,20 @@ import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.take
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         lifecycleScope.launchWhenStarted {
+            // Collect: terminal operator
             makeFlow().collect {
                 println("received $it")
+            }
+            // take: takes only count's emission
+            makeFlow().take(2).collect {
+                println("Received $it")
             }
             println("Reception Completed")
         }
